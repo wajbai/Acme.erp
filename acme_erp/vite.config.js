919447735659d044c17c1725cdf73d430c40ab7b
wajbai <148874074+wajbai@users.noreply.github.com>
2025-06-20 18:52:33 +0530

    Update vite.config.js

--- acme_erp/vite.config.js
+++ acme_erp/vite.config.js
@@ -2,9 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/Acme.erp/',  // ✅ This line is REQUIRED for GitHub Pages
   plugins: [react()],
   server: {
-    host: '0.0.0.0', 
+    host: '0.0.0.0',
     port: 5173,
     open: true
   }
